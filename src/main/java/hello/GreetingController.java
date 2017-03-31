dd2b0b367af2704400a21d2cac4e8bee22d084da
Tapczanq <tapczanqwer@wp.pl>
2017-03-31 18:25:19 +0200

    Initail project

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "dupa";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello";
+}
+    
+    @RequestMapping("/greeting/{name}")
+    public String greeting7(@PathVariable(value="name") String name) {
+        return "Hello " + name;
+}
+}
